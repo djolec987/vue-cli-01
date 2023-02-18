1fe2b8c3c2bd6cd7dc18df75a0435e54292e6a8c
Djordje Cvetkovic <djolec987@gmail.com>
2023-02-18 22:59:45 +0100

    Initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+  </li>
+  <ul v-if="detailsAreVisible">
+    <li><strong>Phone: </strong>{{ phoneNumber }}</li>
+    <li><strong>Email: </strong>{{ emailAddress }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAddress"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "sheena",
+        name: "Sheena Shaw",
+        phone: "3658 998 663",
+        email: "shsh@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
